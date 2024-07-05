9fe4f6c048575802f203df6362b184fcd76deccf
Florian Monfort <fmonfort@enssat.fr>
2024-07-05 14:58:21 +0200

    Format + patch mistake

--- functions/compute_follow_up_persistency.py
+++ functions/compute_follow_up_persistency.py
@@ -8,7 +8,7 @@ def compute_follow_up_persistency(group):
     last_delivery_qta = group["QTA"].iloc[-1]
     interval_days = (PERSISTENZA_THRESHOLD_DATE - datetime.fromisoformat(last_delivery_date)).days
 
-    if interval_days > 30 + 30 * last_delivery_qta:
+    if interval_days > 30 * (1 + last_delivery_qta):
         return 1
     else:
         return 0
--- functions/compute_persistency.py
+++ functions/compute_persistency.py
@@ -3,15 +3,14 @@ from datetime import datetime
 
 def compute_persistency(group):
     count = 0
-    previous_delivery = group["DT_EROG"].iloc[0]
 
     for i in range(1, len(group["DT_EROG"])):
+        previous_delivery = group["DT_EROG"].iloc[i - 1]
         current_delivery = group["DT_EROG"].iloc[i]
         interval_days = (datetime.fromisoformat(current_delivery) - datetime.fromisoformat(previous_delivery)).days
 
         # If the current delivery is older of n days with n = 30 + 30 * QTA of previous delivery
-        if interval_days > (30 + 30 * group["QTA"].iloc[i - 1]):
+        if interval_days > (30 * (1 + group["QTA"].iloc[i - 1])):
             count += 1
-            previous_delivery = current_delivery
 
     return count
