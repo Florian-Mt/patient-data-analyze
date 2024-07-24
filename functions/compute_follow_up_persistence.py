309f5a33739ddc68f6d14dd7ed5f63457e169208
Florian Monfort <fmonfort@enssat.fr>
2024-07-24 17:17:20 +0200

    Rename variable to full English

--- functions/compute_follow_up_persistence.py
+++ functions/compute_follow_up_persistence.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-PERSISTENZA_THRESHOLD_DATE = datetime.fromisoformat("2024-05-09")
+PERSISTENCE_THRESHOLD_DATE = datetime.fromisoformat("2024-05-09")
 
 
 def compute_follow_up_persistence(group):
     last_delivery_date = group["DT_EROG"].iloc[-1]
     last_delivery_qta = group["QTA"].iloc[-1]
-    interval_days = (PERSISTENZA_THRESHOLD_DATE - datetime.fromisoformat(last_delivery_date)).days
+    interval_days = (PERSISTENCE_THRESHOLD_DATE - datetime.fromisoformat(last_delivery_date)).days
 
     if interval_days > 30 * (1 + last_delivery_qta):
         return 1
